0d95ab03bc565a39d622cc5286c88fe8d1d2c7e3
Alberto Calzada <alberto.calzada2@gmail.com>
2024-02-08 22:29:04 +0100

    creando contador de clicks

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,31 +1,45 @@
 import './App.css';
 import logo from './img/counter.png';
+import Contador from './components/counter';
 import Boton from './components/button';
+import { useState } from 'react'; //para actualizar un componente 
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const actionClick = () => {
+    setNumClicks(numClicks + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  }
   return (
     <div className="App">
       <div className='logo-contenedor'>
-        <img 
-        className='logo'
-        src={logo}
-        alt='Logo'
+        <img
+          className='logo'
+          src={logo}
+          alt='Logo'
         />
       </div>
       <div className='contenedor'>
-    <Boton
-    texto={'Click'}
-    isButtonClick={true}
-    actionClick={}
-    
-    />
+        <Contador
+          numClicks={numClicks} />
+        <Boton
+          texto={'Click'}
+          isButtonClick={true}
+          actionClick={actionClick}
 
-<Boton
-    texto={'Reiniciar'}
-    isButtonClick={false}
-    actionClick={}
-    
-    />
+        />
+
+        <Boton
+          texto={'Reiniciar'}
+          isButtonClick={false}
+          actionClick={reiniciarContador}
+
+        />
       </div>
     </div>
   );
--- click-counter/src/components/button.jsx
+++ click-counter/src/components/button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../styles/button.css';
 function Button({ texto, isButtonClick, actionClick }) {
     return (
 
